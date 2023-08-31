ea61ee9b9c804a76643d8a38ff202d52f5dda0c6
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2023-08-31 16:06:33 +0800

    url update

--- signalRAction.js
+++ signalRAction.js
@@ -1,6 +1,37 @@
-var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+var connection = new signalR.HubConnectionBuilder().withUrl("https://3d23-61-222-180-215.ngrok-free.app/chatHub").build();
 connection.start().then(function () {
     console.log("Hub 連線完成");
 }).catch(function (err) {
     alert('連線錯誤: ' + err.toString());
+});
+
+// 更新連線 ID 列表事件
+connection.on("UpdList", function (jsonList) {
+    var list = JSON.parse(jsonList);
+    $("#IDList li").remove();
+    for(i=0; i<list.length; i++)
+    {
+        $("#IDList").append($("<li></li>").attr("class", "list-group-item").text(list[i]));
+    }
+});
+
+// 更新用戶個人連線 ID 事件
+connection.on("UpdSelfID", function (id) {
+    $('#SelfID').html(id);
+});
+
+// 更新聊天內容事件
+connection.on("UpdContent", function (msg) {
+    $("#Content").append($("<li></li>").attr("class", "list-group-item").text(msg));
+});
+
+//傳送訊息
+$('#sendButton').on('click', function() {
+    let selfID = $('#SelfID').html();
+    let message = $('#message').val();
+    let sendToID = $('#sendToID').val();
+    let selfName = $(`#selfName`).val();
+    connection.invoke("SendMessage", selfID, message, sendToID,selfName).catch(function (err) {
+        alert('傳送錯誤: ' + err.toString());
+    });
 });
