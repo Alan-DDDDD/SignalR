e73e6652e6b2caac0fb5ad398a59a2c788424539
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2023-09-01 10:34:44 +0800

    1

--- signalRAction.js
+++ signalRAction.js
@@ -1,10 +1,11 @@
 connect();
 async function connect(){
     localStorage.setItem("currentUrl",location.href)
-    var response = await fetch("https://4d17-61-222-180-215.ngrok-free.app/t");
+    var response = await fetch("https://4d17-61-222-180-215.ngrok-free.app/t/");
     var data = await response.json();
     //open("https://4d17-61-222-180-215.ngrok-free.app/t","_self");
     console.log(data);
+
 }
 var connection = new signalR.HubConnectionBuilder().withUrl("https://4d17-61-222-180-215.ngrok-free.app/chathub").build();
 connection.start().then(function () {
